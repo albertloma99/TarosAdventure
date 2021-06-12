3edc8fa45fa843690d3bec01a01f27ec2f431969
Aitor Garcia Diez <bbminze@gmail.com>
2021-06-12 19:05:27 +0200

    MainMenu

---
+++ Assets/Main Menu/MainMenuBehaviour.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Main_Menu
+{
+    public class MainMenuBehaviour : MonoBehaviour
+    {
+        public GameObject leaderBoard;
+        public GameObject mainMenu;
+
+        public void SwitchLeaderboard()
+        {
+            var active = this.mainMenu.activeSelf;
+            this.mainMenu.SetActive(!active);
+            this.leaderBoard.SetActive(active);
+        }
+
+        public void LoadFirstLevel()
+        {
+            SceneManager.LoadScene(1);
+        }
+
+        public void LoadSecondLevel()
+        {
+            SceneManager.LoadScene(2);
+        }
+
+        public void Exit()
+        {
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#else
+            UnityEngine.Application.Quit();
+#endif
+        }
+    }
+}
